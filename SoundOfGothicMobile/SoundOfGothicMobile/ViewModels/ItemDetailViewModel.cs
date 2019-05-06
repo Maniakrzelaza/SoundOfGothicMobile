23cf5561854dbd39c88c07c09b23f9756265ca35
Maniakrzelaza <maniakrzelaza@gmail.com>
2019-05-06 19:50:32 +0200

    Add project files.

---
+++ SoundOfGothicMobile/SoundOfGothicMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SoundOfGothicMobile.Models;
+
+namespace SoundOfGothicMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
