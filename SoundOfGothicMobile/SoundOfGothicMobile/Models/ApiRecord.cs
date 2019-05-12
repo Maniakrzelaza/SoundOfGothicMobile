5d3f69c40d1f6198fe9aac706c4109a79096bf37
Maniakrzelaza <maniakrzelaza@gmail.com>
2019-05-12 19:11:06 +0200

    custom font

---
+++ SoundOfGothicMobile/SoundOfGothicMobile/Models/ApiRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoundOfGothicMobile.Models
+{
+    public class ApiRecord
+    {
+        public string _id { get; set; }
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public int G { get; set; }
+        public string Source { get; set; }
+    }
+}
---
+++ SoundOfGothicMobile/SoundOfGothicMobile/ViewModels/ApiRecordViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SoundOfGothicMobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SoundOfGothicMobile.ViewModels
+{
+	public class ApiRecordViewModel
+    {
+        public List<ApiRecord> ApiRecords { get; set; }
+        public List<ApiRecord> Data { get; set; }
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { isBusy = value; }
+        }
+        public ApiRecordViewModel()
+        {
+            ApiRecords = new List<ApiRecord>();
+        }
+        public ApiRecordViewModel(List<ApiRecord> records)
+        {
+            ApiRecords = new List<ApiRecord>();
+            ApiRecords.Clear();
+            ApiRecords.AddRange(records);
+        }
+    }
+}
